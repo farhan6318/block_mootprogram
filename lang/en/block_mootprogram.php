e3b11bb8e5d4338ea429d7bebb916be239c10836
Mathew May <mathewm@hotmail.co.nz>
2020-07-02 17:03:58 +0800

    abstract presentations

--- lang/en/block_mootprogram.php
+++ lang/en/block_mootprogram.php
@@ -1,3 +1,15 @@
 <?php
 
 $string['pluginname'] = 'Program highlights';
+$string['editsession'] = 'Edit session';
+$string['discussion'] = 'Discussion';
+$string['session'] = 'Session';
+$string['rightnow'] = 'Right now';
+$string['sponsersolutions'] = 'Sponsor solutions';
+$string['visitsponsers'] = 'Visit Sponsors';
+$string['networkingcafe'] = 'Networking cafe';
+$string['joinnetworking'] = 'Join Networking';
+$string['sponseralt'] = 'Visit our sponsers';
+$string['cafealt'] = 'Visit our networking area';
+$string['navigatesponser'] = 'go to the sponser area';
+$string['navigatecafe'] = 'go to the networking area';
